198332f85669e4d4588cb72791daec7bdecf794d
Jesse DuMond <suttonshiredev@gmail.com>
2020-05-21 23:06:39 -0400

    Initial github CI

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-
 ECHOIF=echo
 ECHOHWADDR=06:06:06:06:06:06
+export XSK_TEST_SCRIPT_DIR=$1
 ip tuntap add name $ECHOIF mode tap
 ip link set dev $ECHOIF address $ECHOHWADDR
-ip link set dev $ECHOIF xdpgeneric obj xdp/xdp_prog_kern.o sec xdp_echo
+ip link set dev $ECHOIF xdpgeneric obj \
+   $XSK_TEST_SCRIPT_DIR/xdp/xdp_prog_kern.o sec xdp_echo
 ip link set dev $ECHOIF up
 
 dune runtest
