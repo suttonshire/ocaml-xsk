f1d38a745fa9fcbd572ce4a89f70449209682070
Jesse DuMond <suttonshiredev@gmail.com>
2020-07-08 20:22:10 -0400

    [bench] formatting

--- bench/udp_pkt_gen.ml
+++ bench/udp_pkt_gen.ml
@@ -31,31 +31,32 @@ type%cstruct udp4 =
   }
 [@@big_endian]
 
-(**
- * [mac_of_string str] parses the mac address [str] into a format suitable for the
- * ethernet cstruct. Throws if [str] does not have the format XX:XX:XX:XX:XX:XX
- *)
+(** * [mac_of_string str] parses the mac address [str] into a format suitable for the *
+    ethernet cstruct. Throws if [str] does not have the format XX:XX:XX:XX:XX:XX *)
 let mac_of_string str =
   str
   |> String.split ~on:':'
-  |> fun l -> if List.length l <> 6 then raise (Invalid_argument ("invalid mac address")) else l
-  |> List.map ~f:(fun s -> String.concat [ "0x"; s ])
-  |> List.map ~f:Int.of_string
-  |> List.map ~f:Char.of_int_exn
-  |> String.of_char_list
+  |> fun l ->
+  if List.length l <> 6
+  then raise (Invalid_argument "invalid mac address")
+  else
+    l
+    |> List.map ~f:(fun s -> String.concat [ "0x"; s ])
+    |> List.map ~f:Int.of_string
+    |> List.map ~f:Char.of_int_exn
+    |> String.of_char_list
 ;;
 
-(**
- * [ip_of_string str] parses the ip address [str] into a format suitable for the
- * ethernet cstruct. Throws if [str] does not have the format XXX.XXX.XXX.XXX.XXX.XXX
- *)
+(** * [ip_of_string str] parses the ip address [str] into a format suitable for the *
+    ethernet cstruct. Throws if [str] does not have the format XXX.XXX.XXX.XXX.XXX.XXX *)
 let ip_of_string str =
   str
   |> String.split ~on:'.'
-  |> fun l -> if List.length l <> 4 then raise (Invalid_argument ("invalid ip address")) else l
-  |> List.map ~f:Int.of_string
-  |> List.map ~f:Char.of_int_exn
-  |> String.of_char_list
+  |> fun l ->
+  if List.length l <> 4
+  then raise (Invalid_argument "invalid ip address")
+  else
+    l |> List.map ~f:Int.of_string |> List.map ~f:Char.of_int_exn |> String.of_char_list
 ;;
 
 let set_pkt ~src_mac ~dst_mac ~src_ip ~dst_ip pkt pos =
@@ -66,7 +67,6 @@ let set_pkt ~src_mac ~dst_mac ~src_ip ~dst_ip pkt pos =
   set_ethernet_src src_ether_addr 0 ether;
   set_ethernet_dst dst_ether_addr 0 ether;
   set_ethernet_ethertype ether 0x800;
-
   (* Create the IP header *)
   let ip = Cstruct.create sizeof_ipv4 in
   let src_ip_addr = ip_of_string (Host_and_port.host src_ip) in
@@ -84,11 +84,10 @@ let set_pkt ~src_mac ~dst_mac ~src_ip ~dst_ip pkt pos =
   let csum = ref 0xFFFF in
   for i = 0 to 9 do
     csum := !csum + Cstruct.BE.get_uint16 ip (2 * i);
-    if (!csum > 0xFFFF) then csum := !csum - 0xFFFF;
+    if !csum > 0xFFFF then csum := !csum - 0xFFFF
   done;
   csum := lnot !csum;
   set_ipv4_csum ip !csum;
-
   (* Create the UDP header *)
   let udp = Cstruct.create sizeof_udp4 in
   let src_port = Host_and_port.port src_ip in
@@ -99,11 +98,9 @@ let set_pkt ~src_mac ~dst_mac ~src_ip ~dst_ip pkt pos =
   set_udp4_len udp (sizeof_udp4 + data_len);
   (* We don't set the csum for now *)
   set_udp4_csum udp 0;
-
   (* Create the data *)
   let buf = Cstruct.create data_len in
   Cstruct.LE.set_uint64 buf 0 (Int64.of_int pos);
-
   let data = Cstruct.concat [ ether; ip; udp; buf ] in
   Base_bigstring.From_bytes.blit
     ~src:(Cstruct.to_bytes data)
@@ -150,7 +147,6 @@ let send_batch batch_size frame_size send txq fd descs =
     let pos = (send + i) land (frame_count - 1) in
     descs.(i).Xsk.Desc.addr <- pos * frame_size
   done;
-  
   let sent = ref 0 in
   while !sent = 0 do
     sent := Xsk.Tx_queue.produce_and_wakeup_kernel txq fd descs ~pos:0 ~nb:batch_size
@@ -195,12 +191,11 @@ let tx cnt frame_size cq socket txq =
     then ()
     else if (* Wait for the socket to be writeable *)
             not (Xsk.Socket.pollout socket 100)
-    then (
-      loop sent consumed)
+    then loop sent consumed
     else (
       (* [tx_batch_size] is not more than the number of free frames remaining in the umem
        * and not more than the [max_batch_size]
-      *)
+       *)
       let tx_batch_size = Int.min max_batch_size (frame_count - (sent - consumed)) in
       let sent =
         (* If [tx_batch_size = 0] then we have transmitted every available umem frame and
@@ -212,8 +207,7 @@ let tx cnt frame_size cq socket txq =
           Xsk.Socket.wakeup_kernel_with_sendto socket;
           sent)
       in
-      let consumed0 = Xsk.Comp_queue.consume cq addrs ~pos:0 ~nb:max_batch_size
-      in
+      let consumed0 = Xsk.Comp_queue.consume cq addrs ~pos:0 ~nb:max_batch_size in
       loop sent (consumed + consumed0))
   in
   loop 0 0
@@ -253,10 +247,7 @@ let command =
       let%map interface = flag "-d" (required string) ~doc:"device Device to transmit on"
       and queue = flag "-q" (required int) ~doc:"queue_id Interface queue to transmit on"
       and frame_size =
-        flag
-          "-f"
-          (optional_with_default 2048 int)
-          ~doc:"n Size of each frame in the umem"
+        flag "-f" (optional_with_default 2048 int) ~doc:"n Size of each frame in the umem"
       and src_ip =
         flag
           "-sip"
@@ -282,7 +273,9 @@ let command =
         flag
           "-dmac"
           (required string)
-          ~doc:"destination_mac Destination mac address to use in the ethernet headers. Format XX:XX:XX:XX:XX:XX"
+          ~doc:
+            "destination_mac Destination mac address to use in the ethernet headers. \
+             Format XX:XX:XX:XX:XX:XX"
       and zero_copy =
         flag "-z" (no_arg_some Xsk.Bind_flag.XDP_ZEROCOPY) ~doc:"Zero copy mode"
       and needs_wakeup =
@@ -293,9 +286,11 @@ let command =
       and cnt =
         flag "-c" (optional_with_default 1_000_000 int) ~doc:"n How many packets to send"
       in
-      let xdp_flags = match zero_copy with
-      | None -> make_xdp_flags None
-      | Some _ -> make_xdp_flags (Some Xsk.Xdp_flag.XDP_FLAGS_DRV_MODE) in
+      let xdp_flags =
+        match zero_copy with
+        | None -> make_xdp_flags None
+        | Some _ -> make_xdp_flags (Some Xsk.Xdp_flag.XDP_FLAGS_DRV_MODE)
+      in
       let bind_flags = make_bind_flags zero_copy needs_wakeup in
       fun () ->
         with_umem frame_size ~f:(fun mem umem (_ : Xsk.Fill_queue.t) comp ->
