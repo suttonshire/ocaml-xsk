64a1cce9a4b63beba4deb6074efe37e4d29f3eae
Jesse DuMond <suttonshiredev@gmail.com>
2020-07-16 08:39:18 -0400

    [rxdrop] cleanup

--- bench/rxdrop.ml
+++ bench/rxdrop.ml
@@ -36,8 +36,11 @@ let with_umem frame_size ~f =
     ~finally:(fun () -> Xsk.Umem.delete umem)
 ;;
 
-module Hist = struct
+module Sampler = struct
+  (** * Store throughput samples for each bin of 1024*1024 packets. We just store 16
+      samples. *)
   let bins = 2 lsl 4
+
   let bin_mask = bins - 1
   let bin_size = 1024 * 1024
 
@@ -56,6 +59,7 @@ module Hist = struct
   ;;
 
   let incr t amount =
+    (* Every 1024 * 1024 frames store the delta t *)
     if t.cnt + amount >= bin_size
     then (
       t.cnt <- t.cnt + amount - bin_size;
@@ -73,21 +77,21 @@ module Hist = struct
     with
     | Some _ -> Stdio.printf "Not enough samples"
     | None ->
-      let max =
+      let max_dt =
         Array.fold t.hist ~init:t.hist.(0) ~f:(fun accum t ->
             Time_stamp_counter.Span.max accum t)
         |> Time_stamp_counter.Span.to_ns
              ~calibrator:(Lazy.force Time_stamp_counter.calibrator)
       in
-      let min =
+      let min_dt =
         Array.fold t.hist ~init:t.hist.(0) ~f:(fun accum t ->
             Time_stamp_counter.Span.min accum t)
         |> Time_stamp_counter.Span.to_ns
              ~calibrator:(Lazy.force Time_stamp_counter.calibrator)
       in
-      let min_pps = Int.to_float bin_size /. (Int63.to_float min /. 1000000000.0) in
-      let max_pps = Int.to_float bin_size /. (Int63.to_float max /. 1000000000.0) in
-      Stdio.printf "[rxdrop] max: %f (pps) min: %f(pps) \n" max_pps min_pps
+      let max_pps = Int.to_float bin_size /. (Int63.to_float min_dt /. 1000000000.0) in
+      let min_pps = Int.to_float bin_size /. (Int63.to_float max_dt /. 1000000000.0) in
+      Stdio.printf "[rxdrop] max: %f (pps) min: %f (pps) \n" max_pps min_pps
   ;;
 end
 
@@ -110,7 +114,7 @@ let do_rx_drop
   in
   let batch_size = 64 in
   let descs = Array.init frame_count ~f:(fun (_ : int) -> Xsk.Desc.create ()) in
-  let hist = Hist.create () in
+  let hist = Sampler.create () in
   if filled <> frame_count
   then (
     let error_string =
@@ -122,7 +126,6 @@ let do_rx_drop
     Or_error.error_string error_string)
   else (
     let rec drop_loop cnt =
-      (* Every 1024 * 1024 frames store the current time *)
       if cnt >= upto
       then ()
       else (
@@ -132,14 +135,16 @@ let do_rx_drop
           then Xsk.Socket.wakeup_kernel_with_sendto socket;
           drop_loop cnt
         | rcvd ->
-          if rcvd < 0 then failwith "fuckup";
+          if rcvd < 0 then failwith "oh dear";
           for i = 0 to rcvd - 1 do
             let desc = Array.unsafe_get descs i in
             Array.unsafe_set addrs i desc.addr;
             if desc.len >= 8
+               (* Load a value from the frame to make sure we go all the way through the
+                  LLC *)
             then ignore (Bigstring.unsafe_get_int64_le_trunc mem ~pos:desc.addr : int)
           done;
-          Hist.incr hist rcvd;
+          Sampler.incr hist rcvd;
           let filled =
             ref (Xsk.Fill_queue.produce_and_wakeup_kernel fill fd addrs ~pos:0 ~nb:rcvd)
           in
@@ -150,7 +155,7 @@ let do_rx_drop
           drop_loop (cnt + rcvd))
     in
     (drop_loop 0 : unit);
-    Hist.print hist;
+    Sampler.print hist;
     Or_error.return ())
 ;;
 
@@ -160,21 +165,13 @@ let rxdrop bind_flags xdp_flags interface queue frame_size cnt =
           do_rx_drop mem umem fill comp socket rx tx frame_size cnt))
 ;;
 
-let tx (_ : string) (_ : int) (_ : int) = ()
-
-let make_xdp_flags mode =
-  match mode with
-  | Some flag -> [ flag; Xsk.Xdp_flag.XDP_FLAGS_UPDATE_IF_NOEXIST ]
-  | None -> [ Xsk.Xdp_flag.XDP_FLAGS_DRV_MODE; Xsk.Xdp_flag.XDP_FLAGS_UPDATE_IF_NOEXIST ]
-;;
-
-let make_bind_flags zero_copy needs_wakeup =
+let make_flags zero_copy needs_wakeup =
   (* Default bind flags are [ XDP_COPY ] *)
   match zero_copy, needs_wakeup with
-  | None, None -> [ Xsk.Bind_flag.XDP_COPY ]
-  | Some zc, None -> [ zc ]
-  | None, Some nw -> [ nw ]
-  | Some zc, Some nw -> [ zc; nw ]
+  | None, None -> [ Xsk.Bind_flag.XDP_COPY ], [ Xsk.Xdp_flag.XDP_FLAGS_SKB_MODE ]
+  | Some zc, None -> [ zc ], [ Xsk.Xdp_flag.XDP_FLAGS_DRV_MODE ]
+  | None, Some nw -> [ nw; Xsk.Bind_flag.XDP_COPY ], [ Xsk.Xdp_flag.XDP_FLAGS_SKB_MODE ]
+  | Some zc, Some nw -> [ zc; nw ], [ Xsk.Xdp_flag.XDP_FLAGS_DRV_MODE ]
 ;;
 
 let command =
@@ -199,11 +196,10 @@ let command =
           ~doc:"n How many packets to receive"
       in
       fun () ->
-        let bf = make_bind_flags zero_copy needs_wakeup in
-        let xdpf = make_xdp_flags None in
-        rxdrop bf xdpf interface queue frame_size cnt
-        |> Or_error.sexp_of_t Unit.sexp_of_t
-        |> Stdio.eprint_s)
+        let bf, xdpf = make_flags zero_copy needs_wakeup in
+        match rxdrop bf xdpf interface queue frame_size cnt with
+        | Error e -> Error.to_string_hum e |> Stdio.prerr_endline
+        | Ok _ -> ())
 ;;
 
 let () = Command.run command
